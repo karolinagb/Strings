48cfc469e8853120a0ea6a344909b5878391965f
x <karolina_bp@hotmail.com>
2022-06-10 15:52:19 -0300

    Validação de nome e sobrenome

--- Usuario.php
+++ Usuario.php
@@ -16,8 +16,20 @@ class Usuario
         //explode() vai retornar um array contendo strings
         $nomeSobrenome = explode(" ", $nome, 2);
 
-        $this->nome = $nomeSobrenome[0];
-        $this->sobrenome = $nomeSobrenome[1];
+        if($nomeSobrenome[0] === ""){
+            $this->nome = "Nome inválido";
+        }
+        else{
+            $this->nome = $nomeSobrenome[0];
+        }
+
+        if($nomeSobrenome[1] === null){
+            $this->sobrenome = "Sobrenome inválido";
+        }
+        else{
+            $this->sobrenome = $nomeSobrenome[1];
+        }
+
     }
 
     public function getNome(): string
